956fd97319af799afc26879bdb21e47d2f7f80b8
Utsob Roy <roy@utsob.me>
2025-11-19 13:59:07 +0600

    dotr 0.5.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/dotr.rb
+++ Formula/dotr.rb
@@ -1,11 +1,11 @@
 class Dotr < Formula
   desc "A dotfiles manager that is as dear as a daughter"
   homepage "https://github.com/uroybd/DotR"
-  version "0.5.3"
+  version "0.5.4"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/uroybd/DotR/releases/download/v0.5.3/dotr-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/uroybd/DotR/releases/download/v0.5.4/dotr-aarch64-apple-darwin.tar.gz"
       sha256 "" # Will be auto-updated by release workflow
     end
   end
